9fe8704e7b7007e004506b7c5d24d47fee943dca
SirTLD <ekanemita@gmail.com>
2023-10-14 22:50:41 -0400

    final commit

--- app/components/welcome-message.tsx
+++ app/components/welcome-message.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-import { BsFillPinMapFill } from 'react-icons/bs'
+import { FaMapMarkedAlt } from 'react-icons/fa'
 
 const WelcomeMessage = () => {
   return (
     <div className='flex flex-col items-center justify-center h-[294px] mt-48 space-y-4 bg-white rounded-md w-full absolute z-50 shadow-xl'>
-      <BsFillPinMapFill size='40px' />
-      <p>Track Globally</p>
+      <FaMapMarkedAlt size='50px' />
+      <p className='font-bold'>Track Globally</p>
     </div>
   )
 }
